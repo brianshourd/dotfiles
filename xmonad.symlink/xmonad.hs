d41231ab3b178038880b2e7528c2e09c831f25fc
Brian Shourd <brian.shourd@gmail.com>
2014-07-03 20:56:25 -0700

    Update at new job

--- xmonad.symlink/xmonad.hs
+++ xmonad.symlink/xmonad.hs
@@ -1,6 +1,6 @@
 import XMonad
 import XMonad.Config.Desktop
-import XMonad.Config.Xfce
+-- import XMonad.Config.Xfce
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Grid
 import XMonad.Layout.Spacing
@@ -10,9 +10,10 @@ import Padding
 
 import qualified Data.Map as M
 
-main = xmonad $ xfceConfig
-	{ focusedBorderColor = "#000000"
-	, normalBorderColor = "#eee8d5"
+main = xmonad $ defaultConfig
+-- main = xmonad $ xfceConfig
+	{ focusedBorderColor = "#90A959"
+	, normalBorderColor = "#151515"
 	, borderWidth = 4
 	, layoutHook = avoidStruts $ myLayoutHook
 	, keys = myKeys <+> keys desktopConfig
@@ -23,29 +24,27 @@ myManageHook = composeAll
    [ className =? "net-minecraft-LauncherFrame"  --> doFloat
    , manageDocks ]
 
-
 myLayoutHook = centered ||| tiled ||| full ||| Grid
-    where  
+    where
         centered = padding 0 175 $ Full
 
-        tiled = spacing 2 $ Tall nmaster delta ratio  
+        tiled = Tall nmaster delta ratio
 
         full = spacing 0 $ noBorders Full
 
-        -- The default number of windows in the master pane  
-        nmaster = 1  
+        -- The default number of windows in the master pane
+        nmaster = 1
 
-        -- Default proportion of screen occupied by master pane  
-        ratio = 5/8  
+        -- Default proportion of screen occupied by master pane
+        ratio = 5/8
 
-        -- Percent of screen to increment by when resizing panes  
-        delta = 5/100  
+        -- Percent of screen to increment by when resizing panes
+        delta = 5/100
 
 myKeys (XConfig {modMask = modm}) = M.fromList $
 	[ ((modm,               xK_p     ), spawn "dmenu_run")
     , ((modm .|. shiftMask, xK_h     ), spawn "thunar /home/brian/")
 	, ((modm .|. shiftMask, xK_Return), spawn "urxvt")
-	, ((modm .|. shiftMask, xK_p     ), spawn "xfce4-appfinder")
 	, ((modm .|. shiftMask, xK_q     ), spawn "xfce4-session-logout")
 	]
 
