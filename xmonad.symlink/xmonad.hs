7c914d52fe612abdb9314261e8b49bddff5257df
Brian Shourd <brian.shourd@gmail.com>
2014-09-28 21:22:58 -0700

    Update for work dotfiles

--- xmonad.symlink/xmonad.hs
+++ xmonad.symlink/xmonad.hs
@@ -1,6 +1,5 @@
 import XMonad
 import XMonad.Config.Desktop
--- import XMonad.Config.Xfce
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Grid
 import XMonad.Layout.Spacing
@@ -11,20 +10,14 @@ import Padding
 import qualified Data.Map as M
 
 main = xmonad $ defaultConfig
--- main = xmonad $ xfceConfig
 	{ focusedBorderColor = "#90A959"
 	, normalBorderColor = "#151515"
 	, borderWidth = 4
 	, layoutHook = avoidStruts $ myLayoutHook
 	, keys = myKeys <+> keys desktopConfig
-    , manageHook = myManageHook
 	}
 
-myManageHook = composeAll
-   [ className =? "net-minecraft-LauncherFrame"  --> doFloat
-   , manageDocks ]
-
-myLayoutHook = centered ||| tiled ||| full ||| Grid
+myLayoutHook = centered ||| tiled ||| full
     where
         centered = padding 0 175 $ Full
 
@@ -45,6 +38,6 @@ myKeys (XConfig {modMask = modm}) = M.fromList $
 	[ ((modm,               xK_p     ), spawn "dmenu_run")
     , ((modm .|. shiftMask, xK_h     ), spawn "thunar /home/brian/")
 	, ((modm .|. shiftMask, xK_Return), spawn "urxvt")
-	, ((modm .|. shiftMask, xK_q     ), spawn "xfce4-session-logout")
+	, ((modm .|. shiftMask, xK_v     ), spawn "/home/brian/bin/gvimTss")
 	]
 
