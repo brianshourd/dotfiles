2eb8bb0f6ec83ee5974f9514987b8149d5837b82
Shourd, Brian (Seattle) <brian.shourd@us.imshealth.com>
2015-02-03 10:10:02 -0800

    Xmonad floating key bindings

--- xmonad.symlink/xmonad.hs
+++ xmonad.symlink/xmonad.hs
@@ -1,5 +1,8 @@
+import Data.Ratio
 import XMonad
+import XMonad.Actions.FloatKeys
 import XMonad.Config.Desktop
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Grid
 import XMonad.Layout.Spacing
@@ -9,7 +12,7 @@ import Padding
 
 import qualified Data.Map as M
 
-main = xmonad $ defaultConfig
+main = xmonad $ ewmh defaultConfig
 	{ modMask = mod4Mask
     , focusedBorderColor = "#90A959"
 	, normalBorderColor = "#151515"
@@ -18,11 +21,12 @@ main = xmonad $ defaultConfig
 	, keys = myKeys <+> keys desktopConfig
 	}
 
-myLayoutHook = centered ||| tiled ||| full
+myLayoutHook = centered ||| htiled ||| vtiled ||| full
     where
         centered = padding 0 175 $ Full
 
-        tiled = Tall nmaster delta ratio
+        htiled = Tall nmaster delta ratio
+        vtiled = Mirror $ Tall nmaster delta ratio
 
         full = spacing 0 $ noBorders Full
 
@@ -37,8 +41,10 @@ myLayoutHook = centered ||| tiled ||| full
 
 myKeys (XConfig {modMask = modm}) = M.fromList $
 	[ ((modm,               xK_p     ), spawn "dmenu_run")
-    , ((modm .|. shiftMask, xK_h     ), spawn "thunar /home/brian/")
 	, ((modm .|. shiftMask, xK_Return), spawn "urxvt")
-	, ((modm .|. shiftMask, xK_v     ), spawn "/home/brian/bin/gvimTss")
+    , ((modm,               xK_d     ), withFocused (keysResizeWindow (-20,0) (1%2,0)))
+    , ((modm,               xK_s     ), withFocused (keysResizeWindow (20,0)  (1%2,0)))
+    , ((modm .|. shiftMask, xK_d     ), withFocused (keysResizeWindow (0,-20) (0,1%2)))
+    , ((modm .|. shiftMask, xK_s     ), withFocused (keysResizeWindow (0,20)  (0,1%2)))
 	]
 
